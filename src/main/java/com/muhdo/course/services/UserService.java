223b37a80cc4f907b0251fafa7f449ab17d95c03
Gabriel Valente <gabrielmcv1@gmail.com>
2023-07-20 22:17:32 +0100

    Exception Handling - findById

--- src/main/java/com/muhdo/course/services/UserService.java
+++ src/main/java/com/muhdo/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.muhdo.course.services;
 
 import com.muhdo.course.entities.User;
 import com.muhdo.course.repositories.UserRepository;
+import com.muhdo.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
